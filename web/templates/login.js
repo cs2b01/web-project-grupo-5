447511905cdd80d7f74272b7808a98fa5de2b2c8
SaidChoquehuanca <39719094+SaidChoquehuanca@users.noreply.github.com>
2019-06-05 13:58:01 -0500

    log

--- web/templates/login.js
+++ web/templates/login.js
@@ -12,37 +12,46 @@ function getdata(){
             contentType: 'application/json',
             data : message,
             dataType:'json',
-            success: function(response){
-                alert(JSON.stringify(response));
-                $('#action').html("");
-                if(response['status']==401){
-                $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>');
-                }else{
-                $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Logeado Correctamente</div>');
-                }
+            //success: function(response){
+            //    alert(JSON.stringify(response));
+            //    $('#action').html("");
+            //    if(response['status']==401){
+            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>');
+            //    }else{
+            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Logeado Correctamente</div>');
+            //    }
 
                 //$('#action').html(response['statusText']);
-            },
-            error: function(response){
-                alert(JSON.stringify(response));
-                $('#action').html("");
-                //$('#action').append('<img width="35" height="35" src="/static/images/logo/fail.png"/>');
-                //$('#action').html(response['statusText']);
-                if(response['status']==401){
-                $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>');
-                }else{
-                $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Logeado Correctamente</div>');
-                }
-            //success: function(response){
-                //alert(JSON.stringify(response));
-            //    $('#action').html("")
-            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Logeado Correctamente</div>');
-                //$
             //},
             //error: function(response){
+            //    alert(JSON.stringify(response));
+            //    $('#action').html("");
+                //$('#action').append('<img width="35" height="35" src="/static/images/logo/fail.png"/>');
+                //$('#action').html(response['statusText']);
+            //    if(response['status']==401){
+            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>');
+            //    }else{
+            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Logeado Correctamente</div>');
+            //    }
+            success: function(response){
                 //alert(JSON.stringify(response));
-            //    $('#action').html("")
-            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>')
-            //}
+                $('#action').html("");
+                if(response['status']==401){
+                    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>');}
+                else{
+                $('#action').append('<div href="/static/presentacion.html" class="text-center"><img src="/static/images/check.png"width="20" height="20"/>Logueado Correctamente<a href="/static/presentacion.html"></a></div>')
+                $('#action').
+                }
+                //$
+            },
+            error: function(response){
+                //alert(JSON.stringify(response));
+                $('#action').html("");
+                if(response['status']==401){
+                    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>');}
+                else{
+                $('#action').append('<div href="/static/presentacion.html" class="text-center"><img src="/static/images/check.png"width="20" height="20"/>Logueado Correctamente<a href="/static/presentacion.html"></a></div>')
+                }
+            }
         });
 }
