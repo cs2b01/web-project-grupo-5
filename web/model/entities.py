711dd04f312062758802b2da6a193033b1bfb32a
Sebastian Q.B <sebastian.quispe.b@utec.edu.pe>
2019-06-18 17:33:22 -0500

    Server config

--- web/model/entities.py
+++ web/model/entities.py
@@ -10,6 +10,14 @@ class User(connector.Manager.Base):
     password = Column(String(12))
     email = Column(String(40))
 
+class Equipo(connector.Manager.Base):
+    __tablename__ = 'Equipo'
+    id = Column(Integer,Sequence(''), primary_key=True)
+    nombre = Column(String(10))
+    puntos= Column(Integer)
+    prox_rival = Column(String(10))
+    mvp = Column(String(20))
+
 class Partidos(connector.Manager.Base):
     __tablename__ = 'Partidos'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
