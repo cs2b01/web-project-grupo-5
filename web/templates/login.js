a4c47185528d99b5e2fb74c72975ce48326804ee
anquisbarr <39910724+anquisbarr@users.noreply.github.com>
2019-06-19 02:42:41 -0500

    Updated imgs from recent matches and future matches

--- web/templates/login.js
+++ web/templates/login.js
@@ -12,27 +12,7 @@ function getdata(){
             contentType: 'application/json',
             data : message,
             dataType:'json',
-            //success: function(response){
-            //    alert(JSON.stringify(response));
-            //    $('#action').html("");
-            //    if(response['status']==401){
-            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>');
-            //    }else{
-            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Logeado Correctamente</div>');
-            //    }
 
-                //$('#action').html(response['statusText']);
-            //},
-            //error: function(response){
-            //    alert(JSON.stringify(response));
-            //    $('#action').html("");
-                //$('#action').append('<img width="35" height="35" src="/static/images/logo/fail.png"/>');
-                //$('#action').html(response['statusText']);
-            //    if(response['status']==401){
-            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Ingrese bien sus datos</div>');
-            //    }else{
-            //    $('#action').append('<div class="text-center"><img src="/static/images/error.png"width="20" height="20"/>Logeado Correctamente</div>');
-            //    }
             success: function(response){
                 //alert(JSON.stringify(response));
                 $('#action').html("");
